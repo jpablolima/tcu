589a6f1c5f638d41531cd848db7d4cfee60e5c04
João Pablo <jp.pablolima@gmail.com>
2024-11-13 06:54:08 -0300

    correção do arquivo server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 
 import app from './src/app.js'
-const port = 3000
+const PORT = 3000
 
 
-app.listen(port,() => {
-    console.log(`Server running in port http:localhost${port}`)
+app.listen(PORT,() => {
+    console.log(`Server running in port http:localhost${PORT}`)
 })
