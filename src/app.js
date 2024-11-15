ea016033744345fdd338ac3647e3e0c2291105b1
João Pablo <jp.pablolima@gmail.com>
2024-11-15 07:13:56 -0300

    buscar seleção por id

--- src/app.js
+++ src/app.js
@@ -6,10 +6,14 @@ app.use(express.json())
 
  const selecoes = [
        { id: 1, selecao: 'Brasil', grupo: 'G'},
-       { id: 1, selecao: 'França', grupo: 'G'},
-       { id: 1, selecao: 'Argentina', grupo: 'G'},
-       { id: 1, selecao: 'Alemanha', grupo: 'G'},
+       { id: 2, selecao: 'França', grupo: 'G'},
+       { id: 3, selecao: 'Argentina', grupo: 'G'},
+       { id: 4, selecao: 'Alemanha', grupo: 'G'},
  ]
+
+ function searchSelecaoPorId(id) {
+    return selecoes.filter(selecao => selecao.id ==  id)
+ }
 
 app.get("/", (req, res) => {
     res.send("Hello world")
@@ -17,6 +21,10 @@ app.get("/", (req, res) => {
 
 app.get("/selecoes",(req,  res) => {
     res.status(200).send(selecoes)
+})
+
+app.get('/selecoes/:id', (req, res) => {
+    res.json(searchSelecaoPorId(req.params.id))
 })
 
 app.post('/selecoes', (req, res) => {
