5efb359f95f7047e144bb47c848d60bfa7a2b98e
João Pablo <jp.pablolima@gmail.com>
2024-11-16 06:40:13 -0300

    delete seleção por  id

--- src/app.js
+++ src/app.js
@@ -15,6 +15,11 @@ app.use(express.json())
     return selecoes.filter(selecao => selecao.id ==  id)
  }
 
+ function searchIndexSelecao(id) {
+    return selecoes.findIndex(selecao => selecao.id == id)  
+ }
+
+
 app.get("/", (req, res) => {
     res.send("Hello world")
 })
@@ -31,4 +36,11 @@ app.post('/selecoes', (req, res) => {
     selecoes.push(req.body)
     res.status(201).send('Seleção cadastrata com sucesso!')
 })
+
+app.delete('/selecoes/:id', (req, res) => {
+    let index = searchIndexSelecao(req.params.id)
+    selecoes.splice(index, 1) 
+    res.send(`Seleção com id ${req.params.id} excluída com sucesso!`)
+})
+
 export default app
