9816669f55d43bee48f03addd501bda599a7fd74
João Pablo <jp.pablolima@gmail.com>
2024-11-20 07:05:30 -0300

    configuração de conexão com banco de dados.

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 
 import app from './src/app.js'
+import conexao from './db/conexao.js'
+
 const PORT = 3000
 
+conexao.connect((erro) => {
+    if(erro) {
+        console.log(erro)
+    } else {
+        console.log("Conexão realizada com sucesso!")
 
-app.listen(PORT,() => {
-    console.log(`Server running in port http:localhost${PORT}`)
+        app.listen(PORT,() => {
+            console.log(`Server running in port http:localhost${PORT}`)
+        })
+   }
 })
+
+
